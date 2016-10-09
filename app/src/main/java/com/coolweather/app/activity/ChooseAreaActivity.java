928f4561addb83cb637dbaf47ac0c6839f24e46f
Chandelier <1289578817@qq.com>
2016-10-09 21:02:22 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/coolweather/app/activity/ChooseAreaActivity.java
+++ app/src/main/java/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -2,7 +2,10 @@ package com.coolweather.app.activity;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -67,6 +70,13 @@ public class ChooseAreaActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(this);
+        if(prefs.getBoolean("city_selected",false)){
+            Intent intent=new Intent(this,WeatherActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.choose_area);
         listView=(ListView)findViewById(R.id.list_view);
@@ -83,6 +93,12 @@ public class ChooseAreaActivity extends Activity {
                 } else if (currentLevel==LEVEL_CITY){
                     selectedCity=cityList.get(position);
                     queryCounties();
+                } else if (currentLevel==LEVEL_COUNTY){
+                    String countyCode=countyList.get(position).getCountyCode();
+                    Intent intent=new Intent(ChooseAreaActivity.this,WeatherActivity.class);
+                    intent.putExtra("county_code",countyCode);
+                    startActivity(intent);
+                    finish();
                 }
             }
         });
--- app/src/main/java/com/coolweather/app/util/Utility.java
+++ app/src/main/java/com/coolweather/app/util/Utility.java
@@ -1,5 +1,8 @@
 package com.coolweather.app.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -7,6 +10,14 @@ import com.coolweather.app.db.CoolWeatherDB;
 import com.coolweather.app.model.City;
 import com.coolweather.app.model.County;
 import com.coolweather.app.model.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by Chandelier on 2016/9/25.
@@ -77,5 +88,46 @@ public class Utility {
             }
         }
         return false;
+    }
+    /**
+     * 解析服务器返回的JSON数据，并将解析出的数据存储到本地
+     */
+    public static void handleWeatherResponse(Context context, String response){
+        try {
+            JSONObject jsonObject=new JSONObject(response);
+            JSONObject data=jsonObject.getJSONObject("data");
+            JSONArray forecast=data.getJSONArray("forecast");
+            String cityName=data.getString("city");
+            Log.d("Utility",cityName);
+            //int airCodee=data.getInt("aqi");
+            //String airCode=airCodee+"";
+            JSONObject array1=forecast.getJSONObject(0);
+            String temp1=array1.getString("high");
+            String temp2=array1.getString("low");
+            String weatherDesp=array1.getString("type");
+            String publishTime=array1.getString("date");
+            saveWeatherInfo(context,cityName,temp1,temp2,weatherDesp,publishTime);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * 将服务器返回的所有天气信息存储到SharedPreferences文件中
+     */
+    public static void saveWeatherInfo(Context context,String cityName,
+                                       String temp1,String temp2,String weatherDesp,
+                                       String publishTime){
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy年M月d日", Locale.CHINA);
+        SharedPreferences.Editor editor= PreferenceManager.getDefaultSharedPreferences
+                (context).edit();
+        editor.putBoolean("city_selected",true);
+        editor.putString("city_name",cityName);
+        //editor.putString("weather_code",weatherCode);
+        editor.putString("temp1",temp1);
+        editor.putString("temp2",temp2);
+        editor.putString("weather_desp",weatherDesp);
+        editor.putString("publish_time",publishTime);
+        editor.putString("current_date",sdf.format(new Date()));
+        editor.commit();
     }
 }
